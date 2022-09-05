b4160a9e9d6357f622336a36ebe04f6e6aab1ed7
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-05 01:21:45 -0300

    Criado o nó no firebase, contendo o email codificado na base64, data e ano, idFirebase, categoria, data, descrição, tipo e valor, criado a classe Movimentacao no pacote model e seus atributos, data, categoria, descricao, tipo e valor, gerando seu construtor e seus getter e setter, inclui o onClick no arquivo activity_despesas, depois implementei o método salvarDespesa na classe DespesasActivity, criado também um atributo movimentação, na classe Movimentação criei o método salvar, para implementar o nó no firebase, criado método mesAnoDataEscolhida na classe DateCustom.

--- app/src/main/java/com/gerardusrocha/organizzeclone/activity/DespesasActivity.java
+++ app/src/main/java/com/gerardusrocha/organizzeclone/activity/DespesasActivity.java
@@ -3,16 +3,19 @@ package com.gerardusrocha.organizzeclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.gerardusrocha.organizzeclone.R;
 import com.gerardusrocha.organizzeclone.helper.DateCustom;
+import com.gerardusrocha.organizzeclone.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,20 @@ public class DespesasActivity extends AppCompatActivity {
         campoDescricao = findViewById(R.id.editDescricao);
 
         campoData.setText(DateCustom.dataAtual());
+
+    }
+
+    public void salvarDespesa(View view) {
+
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setData(data);
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar(data);
 
     }
 }
